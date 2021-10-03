58df166d9a40f9524882aafd60ffe5a3f48f8943
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-10-03 23:41:48 +0100

    Update

--- Frontend/frontend/src/pages/Testimonial/ResultatComponent/ResultatDeRecherche.js
+++ Frontend/frontend/src/pages/Testimonial/ResultatComponent/ResultatDeRecherche.js
@@ -1,6 +1,7 @@
 import React ,{ Component } from "react";
 import './ResultatDeRecherche.css'
 import { Button, Form, Col, Row, Table, Container } from "react-bootstrap";
+import { ProgressBar, Step } from "react-step-progress-bar";
 class  ResultatDeRecherche  extends Component{
      
       
@@ -8,33 +9,42 @@ class  ResultatDeRecherche  extends Component{
           
          return(
             <div className="ResultContainer">
+                
             <Container>
                 <Row>
-                    <Col>first 
-                    <p>
-                       test {this.props.from} 
-                          {this.props.to}
+                    <Col>Ma gare de départ 
+                    <div>
+                        {this.props.from} 
+                         
 
-                    </p>
+                    </div>
+                    <div>
+                        {this.props.dateStart}
+                    </div>
+                    <div>
+                    {this.props.hour}
+                    
+                    </div>
                     </Col>
                     <Col>second
-                     <p>
-                        {this.props.hour}
-                    </p>
-                     <p>
-                        {this.props.places}
-                    </p>
+                   
                     </Col>
-                    <Col>Third 
+                    <Col>Ma gare d'arrivée 
                     <p>
-                        {this.props.option}
+                    {this.props.to}
                     </p>
-                    <p>
-                        {this.props.dateStart}
-                    </p>
+                    
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>
+                            {this.props.places}
+                        </p>
                     </Col>
                 </Row>
             </Container>
+        
         </div>
              )
 
