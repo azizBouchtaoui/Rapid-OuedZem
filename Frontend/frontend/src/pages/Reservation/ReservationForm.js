b406f4d7a780002585428072c5250771322863d2
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-10-05 21:33:55 +0100

    Update

--- Frontend/frontend/src/pages/Reservation/ReservationForm.js
+++ Frontend/frontend/src/pages/Reservation/ReservationForm.js
@@ -1,19 +1,19 @@
  
  
-import React, { useState } from 'react';
+import React from 'react';
  
 import Testimonial from '../Testimonial/Testsimonial';
  
  
 const Reservation = () =>{
  
-        const [selectedOption, setSelectedOption] = useState(null);
+ 
       
         return (
-          <div className="container"> 
+          
           <Testimonial/>
          
-    </div>
+   
     );
 
 }
