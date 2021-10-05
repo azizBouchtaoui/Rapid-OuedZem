2f86c7a3c5a5cec5d84516539942b28a156893c8
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-10-05 21:31:26 +0100

    Update

--- Backend/routers/Reservation.js
+++ Backend/routers/Reservation.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
+ 
 const Ville = require('../model/Villes');
 const Time = require('../model/Time');
-const ResultOfTripe = require('../model/ResultOfTrips')
+ 
 
 router.get('/Villes',(req, res) =>{
     Ville.find({},(err, villes)=>{
