728b1c061fac65ccf9009af04aa4fe173896ac4a
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-10-06 21:43:59 +0100

    Update

--- Frontend/frontend/src/Services.js
+++ Frontend/frontend/src/Services.js
@@ -1,66 +1,52 @@
-import config from './config.json'
- 
+import config from "./config.json";
 
-const baseUrl = config.baseUrl
+const baseUrl = config.baseUrl;
 
-
-export function contactUS(body) {  
-    return callPost(baseUrl+'/rapidOZ/ContactUs', body);
+export function contactUS(body) {
+  return callPost(baseUrl + "/rapidOZ/ContactUs", body);
 }
 
-export function Villes(){
-    return callGet(baseUrl+'/Villes')
+export function Villes() {
+  return callGet(baseUrl + "/Villes");
 }
-export  function Time(){
-    return callGet(baseUrl+'/Time')
-    
+export function Time() {
+  return callGet(baseUrl + "/Time");
 }
 const callGet = (url) => {
-    return fetch(url).then(handleres);
-}
-
-
-
-
+  return fetch(url).then(handleres);
+};
 
 const callPost = (url, body) => {
- 
-    return fetch(url, {       
-        method: 'POST',        
-        body: JSON.stringify(body),
-        headers: { "Content-Type" : "application/json" }
-    }).then(handleres);
-}
-
-
- 
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  }).then(handleres);
+};
 
 const callPut = (url, body) => {
-    return fetch(url, {
-        method: 'PUT',
-        body: JSON.stringify(body),
-        headers: { "Content-Type": "application/json" }
-    }).then(handleres);
-}
+  return fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  }).then(handleres);
+};
 
 const callDelete = (url) => {
-    return fetch(url, {
-        method: 'DELETE'
-    }).then(handleres);
-}
+  return fetch(url, {
+    method: "DELETE",
+  }).then(handleres);
+};
 
 const handleres = (res) => {
- 
-    if (res.ok) {
-        console.log("is OK")
-        return res.json();
+  if (res.ok) {
+    console.log("is OK");
+    return res.json();
+  } else {
+    if (res.status === 404) {
+      return Promise.reject();
+    } else {
+      throw res.json();
     }
-    else {
-        if (res.status === 404) {
-            return Promise.reject();
-        } else {
-            throw res.json();
-        }
-    }
-}
-
+  }
+};
