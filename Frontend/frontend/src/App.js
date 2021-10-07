f947fd9b2fb3e88f456a152603de6804da86800a
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-10-07 20:37:27 +0100

    Update

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,42 +1,38 @@
- import React from 'react';
- import { BrowserRouter as Router, 
-Route,
-Redirect,
-Switch, } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-import Reservation from './pages/Reservation/ReservationForm';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Reservation from "./pages/Reservation/ReservationForm";
 
- 
-import Home from './pages/Home/Home';
- 
-import Testimonial from './pages/Testimonial/Testsimonial';
- 
- 
- 
+import Home from "./pages/Home/Home";
 
-const  App = () => {
+import Testimonial from "./pages/Testimonial/Testsimonial";
+
+const App = () => {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <main>
         <Switch>
-        
           <Route path="/" exact>
-            <Home/>
+            <Home />
           </Route>
           <Route path="/Reservation" exact>
-            <Reservation/>
+            <Reservation />
           </Route>
           <Route path="/Testimonial" exact>
-            <Testimonial/>
+            <Testimonial />
           </Route>
-        
-          <Redirect to="/"/>
+
+          <Redirect to="/" />
         </Switch>
       </main>
-
     </Router>
   );
-}
+};
 
 export default App;
