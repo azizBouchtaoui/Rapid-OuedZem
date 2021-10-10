199b9511bab9d8236736529f645db21b95e0617f
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-10-10 15:53:43 +0100

    Update

--- Frontend/frontend/src/Components/Navbar/Navbar.js
+++ Frontend/frontend/src/Components/Navbar/Navbar.js
@@ -1,17 +1,16 @@
-import React , {useCallback,useEffect,useState} from 'react'
-import './Navbar.css';
-import { NavLink } from 'react-router-dom';
-import $ from 'jquery';
-import { Link } from 'react-scroll';
+import React, { useCallback, useEffect, useState } from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import $ from "jquery";
+import { Link } from "react-scroll";
 const Navbar = () => {
-
-  const [show, setShow] = useState(true); 
+  const [show, setShow] = useState(true);
   const [y, setY] = useState(window.scrollY);
   const handleNavigation = useCallback(
     (e) => {
       const window = e.currentTarget;
       if (y > window.scrollY) {
-        setShow(true);;
+        setShow(true);
       } else if (y < window.scrollY) {
         setShow(false);
       }
@@ -19,7 +18,7 @@ const Navbar = () => {
     },
     [y]
   );
-  
+
   useEffect(() => {
     setY(window.scrollY);
     window.addEventListener("scroll", handleNavigation);
@@ -29,116 +28,106 @@ const Navbar = () => {
     };
   }, [handleNavigation]);
 
-
-
-
-
-  function animation(){
-    
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var activeItemNewAnim = tabsNewAnim.find('.active');
+  function animation() {
+    var tabsNewAnim = $("#navbarSupportedContent");
+    var activeItemNewAnim = tabsNewAnim.find(".active");
     var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
     var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
     var itemPosNewAnimTop = activeItemNewAnim.position();
     var itemPosNewAnimLeft = activeItemNewAnim.position();
     $(".hori-selector").css({
-      "top":itemPosNewAnimTop.top + "px", 
-      "left":itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
+      top: itemPosNewAnimTop.top + "px",
+      left: itemPosNewAnimLeft.left + "px",
+      height: activeWidthNewAnimHeight + "px",
+      width: activeWidthNewAnimWidth + "px",
     });
-    $("#navbarSupportedContent").on("click","li",function(e){
-      $('#navbarSupportedContent ul li').removeClass("active");
-      $(this).addClass('active');
+    $("#navbarSupportedContent").on("click", "li", function (e) {
+      $("#navbarSupportedContent ul li").removeClass("active");
+      $(this).addClass("active");
       var activeWidthNewAnimHeight = $(this).innerHeight();
       var activeWidthNewAnimWidth = $(this).innerWidth();
       var itemPosNewAnimTop = $(this).position();
       var itemPosNewAnimLeft = $(this).position();
       $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px", 
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
+        top: itemPosNewAnimTop.top + "px",
+        left: itemPosNewAnimLeft.left + "px",
+        height: activeWidthNewAnimHeight + "px",
+        width: activeWidthNewAnimWidth + "px",
       });
     });
   }
- useEffect(() => {
-    
+  useEffect(() => {
     animation();
-    $(window).on('resize', function(){
-      setTimeout(function(){ animation(); }, 500);
+    $(window).on("resize", function () {
+      setTimeout(function () {
+        animation();
+      }, 500);
     });
-    
   }, []);
 
-
-
-
   return (
-  <nav className={ ` ${show && 'navbar navbar-expand-lg navbar-mainbg'} `}>
-    
-      <Link className="navbar-brand navbar-logo" to="MainSection"  >
+    <nav className={` ${show && "navbar navbar-expand-lg navbar-mainbg"} `}>
+      <Link className="navbar-brand navbar-logo" to="MainSection">
         Rapid OUEDZEM
       </Link>
-    
-    
-      <button 
+
+      <button
         className="navbar-toggler"
-        onClick={ function(){
-          setTimeout(function(){ animation(); });
+        onClick={function () {
+          setTimeout(function () {
+            animation();
+          });
         }}
-        type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <i className="fas fa-bars text-white"></i>
       </button>
- 
-      <div 
-        className={"collapse navbar-collapse"} id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-            
-            <div className="hori-selector">
-              <div className="left"></div>
-              <div className="right"></div>
-            </div>
-            
-            <li className="nav-item active">
-              <Link className="nav-link" to="MainSection" >
-                <i 
-                className="fas fa-tachometer-alt">
-                </i>Home
-              </Link>
-            </li>           
-            <li className="nav-item">
-              <Link  className="nav-link" to="Services" smooth={true} duration={100} > 
-              <i 
-                className="far fa-clone">
-                </i>Services</Link>
-                
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/testimonial" exact>
-                <i 
-                className="far fa-chart-bar">
-                </i>Testimonial
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="About"   >
-                <i 
-                className="far fa-copy">
-                </i>Contact Us
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="About"  >
-                <i 
-                className="far fa-address-book">
-                </i>About
-              </Link> 
-            </li>
 
+      <div className={"collapse navbar-collapse"} id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <div className="hori-selector">
+            <div className="left"></div>
+            <div className="right"></div>
+          </div>
+
+          <li className="nav-item active">
+            <Link className="nav-link" to="MainSection">
+              <i className="fas fa-tachometer-alt"></i>Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              to="Services"
+              smooth={true}
+              duration={100}
+            >
+              <i className="far fa-clone"></i>Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/testimonial" exact>
+              <i className="far fa-chart-bar"></i>Testimonial
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="About">
+              <i className="far fa-copy"></i>Contact Us
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="About">
+              <i className="far fa-address-book"></i>About
+            </Link>
+          </li>
         </ul>
       </div>
-  </nav>
-  )
-}
+    </nav>
+  );
+};
 export default Navbar;
