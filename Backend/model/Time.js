6b8daa038a7eca0d114e6a155dcbc0c4195f387b
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-10-11 16:02:52 +0100

    Update

--- Backend/model/Time.js
+++ Backend/model/Time.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const Times = mongoose.Schema({
-    time:{
-        type: String,
-        required:true
-    }
+  time: {
+    type: String,
+    required: true,
+  },
+});
 
-})
-
-const Time = mongoose.model('Time',Times,'Time' );
+const Time = mongoose.model("Time", Times, "Time");
 module.exports = Time;
