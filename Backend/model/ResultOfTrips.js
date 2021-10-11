553fae6e67e035f8c3ff5db62f3a72bfa8a6f9a8
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-10-11 16:02:43 +0100

    Update

--- Backend/model/ResultOfTrips.js
+++ Backend/model/ResultOfTrips.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ResultOfTrips = mongoose.Schema({
-    From:{
-        type: String,
-        required: true
-    },
-    To:{
-        type: String,
-        required: true
-    },
-    Option:{
-        type: String,
-        required: true
-    },
-    DateOfDeparture :{
-        type: String,
-        required: true
-    },
-    DateReturn:{
-        type: String,
-        required: false
-    },
-    Time:{
-        type: String,
-        required: true
-    },
-    Places:{
-        type: String,
-        required: true
-    }
-} )
+  From: {
+    type: String,
+    required: true,
+  },
+  To: {
+    type: String,
+    required: true,
+  },
+  Option: {
+    type: String,
+    required: true,
+  },
+  DateOfDeparture: {
+    type: String,
+    required: true,
+  },
+  DateReturn: {
+    type: String,
+    required: false,
+  },
+  Time: {
+    type: String,
+    required: true,
+  },
+  Places: {
+    type: String,
+    required: true,
+  },
+});
