0c710f1a2b2f62f043e8ff8c57fadc7c6ef87ebb
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-11-14 20:56:37 +0100

    Update

--- Frontend/frontend/src/pages/Home/Home.js
+++ Frontend/frontend/src/pages/Home/Home.js
@@ -1,43 +1,29 @@
-
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Cards from "../../Components/Cards/Card";
 import MainSection from "../../Components/MainSection/MainSection";
-import Footer from "../../Components/Footer/Footer"
-import Services from "../../Components/Services/Services"; 
-import { useInView } from 'react-intersection-observer';
-import './Home.css';
- 
- 
+import Footer from "../../Components/Footer/Footer";
+import Services from "../../Components/Services/Services";
+import { useInView } from "react-intersection-observer";
+import "./Home.css";
 
- const Home = () => {
-  
-  
-     return (
-      
-      
-<>
-     
-   
-   <section  id="MS"  >
-        
-            <MainSection   />
-        </section>   
-          <section id="CRDS"  >
-           <Cards  className="crds" />
-        </section>
-         
-          <section id="SRVS"   >
-            
-              
-            <Services className="srvs" />
-        </section  >
-             
-           <section id="FTR">
+const Home = () => {
+  return (
+    <>
+      <section id="MS">
+        <MainSection />
+      </section>
+      <section id="CRDS">
+        <Cards className="crds" />
+      </section>
 
-            <Footer  className="ftr" />
-           </section>
- 
-         </>
-     )
- }
- export default Home;
+      <section id="SRVS">
+        <Services className="srvs" />
+      </section>
+
+      <section id="FTR">
+        <Footer className="ftr" />
+      </section>
+    </>
+  );
+};
+export default Home;
