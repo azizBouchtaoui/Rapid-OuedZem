60f51e745a008d7788160431777efbcabb990a97
aziz bouchtaoui <m.bouchtaouiaziz@gmail.com>
2021-11-22 15:34:33 +0100

    Update

--- Frontend/frontend/src/pages/Home/Home.js
+++ Frontend/frontend/src/pages/Home/Home.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Cards from "../../Components/Cards/Card";
 import MainSection from "../../Components/MainSection/MainSection";
 import Footer from "../../Components/Footer/Footer";
-import Services from "../../Components/Services/Services";
-import { useInView } from "react-intersection-observer";
+import Services from "../../Components/Services/Services"; 
 import "./Home.css";
 
 const Home = () => {
